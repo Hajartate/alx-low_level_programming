31ec9afcdfd0ae0efb49b9873385937284c34878
hajar tate <hajartate2004@gmail.com>
2023-09-16 12:34:22 -0700

    all

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,27 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the int to extract the last digit from 
+ *
+ * @i: integer
+ *
  * Return: value of the last digit
  */
-int print_last_digit(int n)
-{
-	int a;
 
-	if (n < 0)
-		n = -n;
-	if (a < 0)
-		a = -a;
-	_putchar(a +'0');
-	return (a);
+int print_last_digit(int i)
+{
+	int r;
+
+	r = i % 10;
+	if (r >= 0)
+	{
+		_putchar('0' + r);
+		return (r);
+	}
+	else
+	{
+		_putchar('0' - r);
+		return (-r);
+	}
+	_putchar('\n');
 }
+
